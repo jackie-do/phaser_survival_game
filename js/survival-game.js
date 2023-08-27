c172b604b6c58053ab63b00357fa7097c18519f2
Jackie Do <jackie_do@outlook.com>
2023-08-27 08:50:59 +0700

    Init project

---
+++ js/survival-game.js
@@ -0,0 +1,29 @@
+const config = {
+  width:512,
+  height:512,
+  backgroundColor: '#999999',
+  type: Phaser.AUTO,
+  parent: 'survival-game',
+  scene:[],
+  scale: {
+    zoom:2,
+  },
+  physics: {
+    default: 'matter',
+    matter: {
+      debug:false,
+      gravity:{y:0},
+    }
+  },
+  plugins: {
+    scene:[
+      {
+        plugin: PhaserMatterCollisionPlugin.default,
+        key: 'matterCollision',
+        mapping: 'matterCollision'
+      }
+    ]
+  }
+}
+
+new Phaser.Game(config);
