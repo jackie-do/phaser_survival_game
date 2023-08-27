46a4e42051291bfb93be6a2fb96509d9a5ac1c4b
Jackie Do <jackie_do@outlook.com>
2023-08-27 09:08:58 +0700

    add MainScene

--- js/survival-game.js
+++ js/survival-game.js
@@ -1,10 +1,12 @@
+import MainScene from "./MainScene.js";
+
 const config = {
   width:512,
   height:512,
   backgroundColor: '#999999',
   type: Phaser.AUTO,
   parent: 'survival-game',
-  scene:[],
+  scene:[MainScene],
   scale: {
     zoom:2,
   },
