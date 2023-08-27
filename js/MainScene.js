ef461796bf115a338578743f4322e01c78cd0fd9
Jackie Do <jackie_do@outlook.com>
2023-08-27 11:38:28 +0700

    Add a tiled map with map.json config file

--- js/MainScene.js
+++ js/MainScene.js
@@ -6,16 +6,18 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log("preload");
-
     Player.preload(this);
+    this.load.image("tiles", "assets/images/RPG Nature Tileset.png")
+    this.load.tilemapTiledJSON("map", "assets/images/map.json")
   }
 
   create() {
-    console.log("create");
-    // this.player = new Phaser.Physics.Matter.Sprite(this.matter.world, 0, 0, 'female', 'townsfolk_f_idle_1');
-    this.player = new Player({scene: this, x: 0, y: 0, texture: 'female', frame: 'townsfolk_f_idle_1'});
-    this.text_player = new Player({scene: this, x: 100, y: 100, texture: 'female', frame: 'townsfolk_f_idle_1'});
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("RPG Nature Tileset", "tiles", 32, 32, 0, 0);
+    const layer1 = map.createLayer("Tile Layer 1", tileset, 0, 0);
+
+
+    this.player = new Player({scene: this, x: 0, y: 0, texture: "female", frame: "townsfolk_f_idle_1"});
     Player.controlMoving(this, this.player)
     // this.player.inputKeys = this.input.keyboard.addKeys({
     //   up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -26,8 +28,6 @@ export default class MainScene extends Phaser.Scene {
   }
 
   update() {
-    console.log("update")
-
     this.player.update();
   }
 }
